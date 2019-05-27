e46f06989a6f6031e944c401dd85854a838f6f5f
Lee min hye <dlalsgp001@naver.com>
2019-05-27 16:06:57 +0900

    add bodyparer module

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const request = require('request')
 
 router.get("/api/getWeather", (req, res) => printWeather(req, res))
 
 module.exports = router
 
 const printWeather = (req, res) => {
-    res.send('hihihi')
+    let options = {
+        uri: "https://api2.sktelecom.com/weather/current/minutely?lat=36.1234&lon=127.1234",
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'appKey': '8290775c-9cc8-4d95-a6d8-19a3805407ff'
+        },
+        json:true
+
+    }
+    let result = request(options)
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const request = require('request')
 
 const app = express()
 const PORT = process.env.PORT || 4000
@@ -8,23 +7,17 @@ const PORT = process.env.PORT || 4000
 const routes = require('./routes.js')
 app.use('/', routes)
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
 app.use(express.static(path.join(__dirname, '..', 'public/')))
 
-let options = {
-    uri: "https://api2.sktelecom.com/weather/current/minutely?lat=36.1234&lon=127.1234",
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json;charset=UTF-8',
-        'appKey': '8290775c-9cc8-4d95-a6d8-19a3805407ff'
-    },
-    json:true
 
-};
 
-request(options, (err, res, body) => {
-    console.log(body.weather["minutely"])
-})
 
 app.listen(PORT, () => {
     console.log(`Check out the app at http://localhost:${PORT}`);
