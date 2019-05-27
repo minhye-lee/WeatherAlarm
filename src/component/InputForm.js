e67200a110725440330a820279ce0029c45ffd9a
Lee min hye <dlalsgp001@naver.com>
2019-05-28 04:22:12 +0900

    complete inputform component layout

--- src/component/InputForm.js
+++ src/component/InputForm.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react'
 import FormWrapper from './FormWrapper'
 
-import { Form, Button} from "react-bootstrap"
+import { Form, Row, Col, Button} from "react-bootstrap"
 
 class InputForm extends Component {
     render() {
         return (
             <FormWrapper>
                 <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
+                    <Form.Group as={Col} controlId="Address-city">
+                        <Form.Label>City</Form.Label>
+                        <Form.Control type="email" placeholder="시/도" />
                     </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                    <Form.Group as={Col} controlId="Address-county">
+                        <Form.Label>County</Form.Label>
+                        <Form.Control type="email" placeholder="시/군/구" />
                     </Form.Group>
-                    <Form.Group controlId="formBasicChecbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                    <Form.Group as={Col} controlId="Address-village">
+                        <Form.Label>Village</Form.Label>
+                        <Form.Control type="email" placeholder="읍/면/동" />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formGridState">
+                        <Form.Label>Gender</Form.Label>
+                        <Form.Control as="select">
+                            <option>남</option>
+                            <option>여</option>
+                        </Form.Control>
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
